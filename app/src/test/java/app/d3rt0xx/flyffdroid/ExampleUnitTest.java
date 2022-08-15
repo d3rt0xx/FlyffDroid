24e570ce138dabb703b02e1311cdec40aded6cc0
d3rt0xx <98334773+d3rt0xx@users.noreply.github.com>
2022-08-15 20:28:25 +0200

    Deleted comments

--- app/src/test/java/app/d3rt0xx/flyffdroid/ExampleUnitTest.java
+++ app/src/test/java/app/d3rt0xx/flyffdroid/ExampleUnitTest.java
@@ -1,14 +1,9 @@
 package app.d3rt0xx.flyffdroid;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
