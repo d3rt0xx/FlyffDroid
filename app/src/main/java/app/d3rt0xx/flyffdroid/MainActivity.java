975f23a906b1f134364ac5c027cf21a5a5afa7ab
d3rt0xx <98334773+d3rt0xx@users.noreply.github.com>
2023-01-08 16:20:06 +0100

    Fix for devices with notch

--- app/src/main/java/app/d3rt0xx/flyffdroid/MainActivity.java
+++ app/src/main/java/app/d3rt0xx/flyffdroid/MainActivity.java
@@ -42,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setLogo(R.mipmap.ic_launcher_clean);
         setContentView(R.layout.activity_main);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
 
         fullScreenOff();
 
